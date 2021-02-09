60895ea60ec265e2ef01344e768eed3f65835a50
milk <gongzhenyu40@qq.com>
2021-02-09 23:43:30 +0800

    ajax

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -9,14 +9,40 @@ function ajax(options) {
         },
         ...options
     }
+    options.query && (options.url += queryParse(options.query));
 
     const xhr = new XMLHttpRequest();
-
+    
+    options.onprogress && (xhr.upload.onprogress = e => options.onprogress(e));
     xhr.onload = function () {
         options.success(this.responseText);
     }
 
     xhr.open(options.method, options.url, true);
 
-    xhr.send();
+    let data = null;
+    options.data && (data = bodyParse(options.data));
+
+    xhr.send(data);
+}
+
+function queryParse(obj) {
+    let arr = [];
+    for (const key in obj) {
+        arr.push(`${key}=${obj[key]}`);
+    }
+    const str = arr.join('&');
+    if (str) {
+        return '?' + str;
+    } else {
+        return ''
+    }
+}
+
+function bodyParse(obj) {
+    const formData = new FormData();
+    for (const key in obj) {
+        formData.append(key, obj[key]);
+    }
+    return formData;
 }
