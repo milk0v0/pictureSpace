5fcd4be3a1a0d95073e10be0275b6e436f8e5efc
milk <gongzhenyu40@qq.com>
2021-02-20 10:18:23 +0800

    init

---
+++ staticServer.js
@@ -0,0 +1,10 @@
+const Koa = require('koa');
+const KoaStaticCache = require('koa-static-cache');
+const app = new Koa();
+
+app.use(KoaStaticCache('./public', {
+    gzip: true,
+    dynamic: true
+}));
+
+app.listen(3000);
