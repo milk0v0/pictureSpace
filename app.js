fbb2f0efceca0508708f12bca8d56ad1fcb2081a
milk <gongzhenyu40@qq.com>
2021-02-22 23:39:33 +0800

    entry

--- app.js
+++ app.js
@@ -2,16 +2,10 @@ const Koa = require('koa');
 const KoaRouter = require('koa-router');
 const body = require('./middlewares/body');
 const koaJwt = require('koa-jwt');
-const KoaStaticCache = require('koa-static-cache');
 const contollers = require('./middlewares/contollers');
 
 const app = new Koa();
 const router = new KoaRouter();
-
-app.use(KoaStaticCache('./public', {
-    gzip: true,
-    dynamic: true
-}));
 
 app.use(koaJwt({ secret: 'milk' }).unless({
     path: [/^\/register/, /^\/login/]
@@ -26,4 +20,4 @@ router.post('/upLoad', body('./public/upLoad'), contollers.upLoad);
 
 app.use(router.routes());
 
-app.listen(8080);
+app.listen(3000);
