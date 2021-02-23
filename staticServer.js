347aed5021d84547879d6ff39d2c89f721b95e0f
milk <gongzhenyu40@qq.com>
2021-02-23 23:54:07 +0800

    staticServer

--- staticServer.js
+++ staticServer.js
@@ -1,4 +1,5 @@
 const Koa = require('koa');
+const http = require('http');
 const KoaStaticCache = require('koa-static-cache');
 const app = new Koa();
 
@@ -6,5 +7,37 @@ app.use(KoaStaticCache('./public', {
     gzip: true,
     dynamic: true
 }));
+
+app.use(ctx => {
+    // 如果由 /api 开头的，就走这里的逻辑
+    const { url, method, headers } = ctx;
+    if (url.startsWith('/api')) {
+        let data;
+        const options = {
+            protocol: 'http:',
+            hostname: '127.0.0.1',
+            port: 3000,
+            path: url.replace(/^\/api/, ''),
+            method,
+            headers,
+        }
+        const req = http.request(options, res => {
+            console.log(1);
+            res.on('data', chunk => {
+                data += chunk;
+                console.log(`BODY: ${chunk}`);
+            })
+            res.on('end', () => {
+                console.log('完成');
+            })
+        })
+        req.on('error', (e) => {
+            console.error(`problem with request: ${e.message}`);
+        });
+        req.end();
+        // console.log(data);
+        // ctx.body = data
+    }
+});
 
 app.listen(8080);
